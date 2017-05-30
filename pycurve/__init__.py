11747d18b49f024adde5da493ee28e532589cb56
Ordarb <ordnas.nuarb@gmail.com>
2017-05-30 09:23:47 +0200

    Update __init__.py

--- pycurve/__init__.py
+++ pycurve/__init__.py
@@ -1,18 +1,19 @@
 __title__ = 'pycurve'
-__author__ = 'SandroBraun'
-__copyright__ = 'Copyright 2017'
+__author__ = 'Sandro Braun'
+__copyright__ = 'Copyright 2017, Ordarb'
+__version__ = '0.1.0'
 
 
 from .curve import Curve
+from .data import Data
 
-
-import logging
+import logging as _logging
 
 try:  # Python 2.7+
-    from logging import NullHandler
+    from logging import NullHandler as _NullHandler
 except ImportError:
-    class NullHandler(logging.Handler):
+    class _NullHandler(_logging.Handler):
         def emit(self, record):
             pass
 
-logging.getLogger(__name__).addHandler(NullHandler())
+_logging.getLogger(__name__).addHandler(_NullHandler())
