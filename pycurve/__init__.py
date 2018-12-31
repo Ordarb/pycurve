de58a9969c693d728a61166f7f947d43ec84904e
Ordarb <ordnas.nuarb@gmail.com>
2018-12-31 13:19:24 +0100

    Update __init__.py

--- pycurve/__init__.py
+++ pycurve/__init__.py
@@ -1,19 +1,4 @@
-__title__ = 'pycurve'
-__author__ = 'Sandro Braun'
-__copyright__ = 'Copyright 2017, Ordarb'
-__version__ = '0.1.0'
-
-
-from .curve import Curve
-from .data import Data
-
-import logging as _logging
-
-try:  # Python 2.7+
-    from logging import NullHandler as _NullHandler
-except ImportError:
-    class _NullHandler(_logging.Handler):
-        def emit(self, record):
-            pass
-
-_logging.getLogger(__name__).addHandler(_NullHandler())
+from .curve import Charting
+from .parametric import Parametric
+from .spline import SmoothingSpline
+from .curve import nelson_siegel, svensson, svensson_adjusted, boerk_christensen
