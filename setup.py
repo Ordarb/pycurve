0c3ad36a7a24fb6b66291c4060c4dbf0edeca765
Ordarb <ordnas.nuarb@gmail.com>
2018-12-31 13:18:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,20 @@
-
-'''
-Yield Curve Estimation
-'''
-
-import sys
-import os
 import codecs
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
-packages = ['pycurve']
+packages = find_packages(include=['pycurve', 'pycurve.*'])
 
-with codecs.open('README.txt','r','utf-8') as f:
+with codecs.open('README.txt','r', 'utf-8') as f:
     readme = f.read()
 
 setup(
-    name                = 'pycurve',
-    author              = 'Sandro Braun',
-    author_email        = 'ordnas.nuarb@gmail.com',
-    version             = '0.1.0',
-    description         = 'Yield Curve Estimation - parametric and smoothing splines',
-    url                 = 'No Homepage',
-    long_description    = readme,
-    packages            = packages
-    )
+    name='NewsMeta',
+    author='Quassel',
+    author_email='sandro.braun@quassel.li',
+    version='0.1',
+    description='NewsMeta - Meta Data Enrichment',
+    url='www.quassel.li',
+    keywords=['data', 'news', 'meta', 'nlp'],
+    long_description=readme,
+    packages=packages
+)
